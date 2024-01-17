3aa3dde7d1b5bf952295560a1c732dce9a579fa6
toris-dev <ironjustlikethat@gmail.com>
2024-01-17 22:59:06 +0900

    react 로 퀴즈앱 만들기!!
    next.js 와 react의 차이점을 알아보기 위해 react 로 만들어보기

---
+++ react-quiz-app/src/components/AppButton/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppButton = () => {
+  return <div>AppButton</div>;
+};
+
+export default AppButton;
---
+++ react-quiz-app/src/constans/index.tsx
@@ -0,0 +1,7 @@
+export const totalQuestions = 10;
+
+export enum Difficutlty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
