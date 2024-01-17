96745b78d89e1a8b30ccfb074f44352e398eb6a7
toris-dev <ironjustlikethat@gmail.com>
2024-01-18 04:54:23 +0900

    pageRouter images 최적화
    도메인 등록

--- markdown-app/next.config.js
+++ markdown-app/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    forceSwcTransforms: true,
+  images: {
+    domains: ["img1.daumcdn.net"],
   },
 };
 
