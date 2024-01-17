d153ca97c289825a955fb3dc4de612bb5c2c0efb
toris-dev <ironjustlikethat@gmail.com>
2024-01-18 04:55:51 +0900

    Feat: AppButton Component 추가

--- react-quiz-app/src/components/AppButton/index.tsx
+++ react-quiz-app/src/components/AppButton/index.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@chakra-ui/react";
 import React from "react";
 
-const AppButton = () => {
-  return <div>AppButton</div>;
+export interface AppButtonProps {
+  value: string;
+  colorScheme: string;
+  variant: string;
+  className?: string;
+  disabled: boolean;
+  width?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onClick: (e?: any) => void;
+}
+
+const AppButton: React.FC<AppButtonProps> = ({
+  value,
+  colorScheme,
+  variant,
+  className,
+  disabled,
+  width,
+  onClick,
+}) => {
+  return (
+    <Button
+      colorScheme={colorScheme}
+      variant={variant}
+      className={className}
+      disabled={disabled}
+      width={width}
+      onClick={onClick}
+    >
+      {value}
+    </Button>
+  );
 };
 
 export default AppButton;
