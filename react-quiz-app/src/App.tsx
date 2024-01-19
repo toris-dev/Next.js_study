60157e24a9226a9108aafa758acad54d490070f8
toris-dev <ironjustlikethat@gmail.com>
2024-01-19 18:35:21 +0900

    Feat: question hook, component 불러오고 관리
    간단하게 만들기위해 App.tsx 한곳에 만들었다.

--- react-quiz-app/src/App.tsx
+++ react-quiz-app/src/App.tsx
@@ -1,35 +1,170 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { IQuestion, IUserAnswer } from "./types";
+import { getQuestionList } from "./services/fetchQuestions";
+import { Difficutlty, totalQuestions } from "./constans";
+import AppSpinner from "./components/Spinner";
+import { Box, Heading } from "@chakra-ui/react";
+import AppButton from "./components/AppButton";
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [questions, setQuestions] = useState<IQuestion[]>([]);
+  const [userAnswer, setUserAnswer] = useState<IUserAnswer[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  console.log(startQuiz, loading);
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const questionListing = await getQuestionList(
+        totalQuestions,
+        Difficutlty.EASY
+      );
+      setQuestions(questionListing);
+      console.log(questionListing);
+      setLoading(false);
+    };
+    fetchQuestions();
+    console.log(questions, loading);
+  }, []);
 
+  const startQuizGame = (): void => {
+    setStartQuiz(true);
+  };
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    if (gameOver) return;
+    const chosenAnswer = e.currentTarget.innerText;
+    const correct = questions[questionNumber]?.correct_answer === chosenAnswer;
+
+    // 정답이라면
+    if (correct) setScore((previous) => previous + 1);
+
+    const answerObject = {
+      question: questions[questionNumber]?.question,
+      answer: chosenAnswer,
+      correct,
+      correctAnswer: questions[questionNumber]?.correct_answer,
+    };
+
+    setUserAnswer((previous) => [...previous, answerObject]);
+  };
+  const nextQuestion = (): void => {
+    const nextQuestion = questionNumber + 1;
+    if (totalQuestions === nextQuestion) {
+      setGameOver(true);
+    }
+    setQuestionNumber(nextQuestion);
+  };
+  const replayQuiz = (): void => {
+    setStartQuiz(false);
+    setGameOver(false);
+    setQuestionNumber(0);
+    setScore(0);
+    setUserAnswer([]);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="App">
+      {loading && (
+        <div className="app-spinner">
+          <AppSpinner
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="purple"
+            size="lg"
+            thickness="5px"
+          />
+        </div>
+      )}
+
+      {userAnswer.length === questionNumber &&
+      !gameOver &&
+      !loading &&
+      !startQuiz ? (
+        <div>
+          <Box boxShadow="base" p="6" rounded="md" bg="white" maxW="560px">
+            <Heading as="h2" size="lg" mb={2}>
+              퀴즈 앱
+            </Heading>
+            <p>
+              참 또는 거짓을 대답할 수 있는 ${totalQuestions}개의 질문이
+              제시됩니다.
+            </p>
+            <AppButton
+              colorScheme="purple"
+              variant="solid"
+              onClick={startQuizGame}
+              value="Start Quiz Game"
+            />
+          </Box>
+        </div>
+      ) : null}
+      {!loading && !gameOver && startQuiz && (
+        <Box boxShadow="base" p="6" rounded="md" bg="white" maxW="560px">
+          <QuestionCard
+            questions={questions[questionNumber].question}
+            category={questions[questionNumber].category}
+            checkAnswer={checkAnswer}
+            totalQuestions={totalQuestions}
+            questionNumber={questionNumber}
+          />
+          <AppButton
+            disabled={
+              userAnswer.length === questionNumber + 1 &&
+              questionNumber !== totalQuestions
+                ? false
+                : true
+            }
+            colorScheme="purple"
+            variant="solid"
+            onClick={nextQuestion}
+            value="Next Question"
+            className="next-button"
+            width="full"
+          />
+        </Box>
+      )}
+
+      {gameOver && (
+        <>
+          <Box boxShadow="base" p="6" rounded="md" bg="white" maxW="560px">
+            <Box mb={4}>
+              <Box fontWeight="bold" as="h3" fontSize="4xl">
+                GameOver!
+              </Box>
+              <Box as="h3" fontSize="xl">
+                Your Score is ${score}/${totalQuestions}
+              </Box>
+            </Box>
+
+            {userAnswer.map((answer, index) => (
+              <Box key={index}>
+                <div className="answer-list">
+                  <Box fontSize="md" fontWeight="bold">
+                    Q.
+                    <p dangerouslySetInnerHTML={{ __html: answer.question }} />
+                  </Box>
+                  <ul>
+                    <li>You answered: {answer.answer}</li>
+                    <li>Correct: answer: ${answer.correctAnswer}</li>
+                  </ul>
+                </div>
+              </Box>
+            ))}
+            <AppButton
+              colorScheme="purple"
+              variant="solid"
+              onClick={replayQuiz}
+              value="Replay Quiz"
+              width="full"
+            />
+          </Box>
+        </>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
