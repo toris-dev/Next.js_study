a240f6c9423a839b5fb82efd05e5a8480ff4e1c2
toris-dev <ironjustlikethat@gmail.com>
2024-03-05 05:14:48 +0900

    Feat: next auth session redirect

--- next-page-weather/pages/index.tsx
+++ next-page-weather/pages/index.tsx
@@ -1,11 +1,24 @@
 import AuthForm from "@/components/AuthForm";
 import Layout from "@/components/Layout";
 import WelcomeContent from "@/components/WelcomeContent";
+import { getSession } from "next-auth/react";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/weather");
+      } else {
+        console.log("no session");
+      }
+    });
+  }, [router]);
   return (
     <>
       <Layout>
